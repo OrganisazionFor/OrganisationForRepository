d0efa4a1a0b862a36a40e69c80d58ec950bfba6c
w00her <rodon@protonmail.ch>
2024-02-16 10:50:26 +0200

    added def run

--- bot.py
+++ bot.py
@@ -1,1 +1,59 @@
-﻿
+﻿import pickle
+from classes import *
+
+
+class Bot:
+    def __init__(self):
+        pass
+
+   
+
+    def run(self):
+
+        book = AddressBook()
+
+        commands = {
+
+
+
+
+            # New contact, phone, birthday - - - > 1 Alex 1111111111 31.12.1700
+            "1": add_contact_phone_birthday,
+            "2": add_contact,                # New contact                  - - - > 2 Alex
+            "3": add_phone,                  # Add phone                    - - - > 3 Alex 2222222222
+            # Add birthday                 - - - > 4 Alex 31.12.1700
+            "4": add_birthday,
+            "5": edit_name,                  # Edit name                    - - - > 5 Alex Alexandr
+            # # Edit phone                   - - - > 6 Alexandr 2222222222 3333333333
+            "6": edit_phone,
+            # # Edit birthday                - - - > 7 Alexandr 31.12.1701
+            '7': edit_birthday,
+            "8": days_to_birthday,           # Day to birthday              - - - > 8 Alexandr
+            "search": search,                     # Search                       - - - > 9 text
+            "remove": remove,
+            "show all": show_all,                  # Show all contact            - - - > show all
+            'help': help,                      # Commands list               - - - > help
+            # # Good bye!                   - - - > good bye / exit / close
+            'good bye': good_bye,
+
+        }
+        try:
+            with open(FILE_NAME, 'rb') as fh:
+                read_book = pickle.load(fh)
+                book.data = read_book
+        except:
+            print('New phone book has been created\n')
+
+        while True:
+            try:
+                user_input = console_input()
+                function, *data = parser(user_input, commands)
+                result = function(book, data)
+
+                if result is not None:
+                    print(result)
+                if result == 'Good bye!':
+                    break
+
+            except:
+                print('\n Check your input! \n')
